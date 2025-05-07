8fa276b424cad55bbfbc7c24272859f74fe01e28
rezwan <rezwan.dev@outlook.com>
2025-05-07 16:22:33 +0600

    change: place error transformation logic out of the observer function in the 'available-place' component
    - modify 'ngOnInit' method

--- src/app/places/available-places/available-places.component.ts
+++ src/app/places/available-places/available-places.component.ts
@@ -1,6 +1,6 @@
 import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs';
+import { catchError, map, throwError } from 'rxjs';
 
 import { Place } from '../place.model';
 import { PlacesComponent } from '../places.component';
@@ -26,15 +26,18 @@ export class AvailablePlacesComponent implements OnInit {
     this.isFetching.set(true);
     const subscription = this.httpClient.get<{ places: Place[] }>('http://localhost:3000/places')
       .pipe(
-        map((resData) => resData.places)
+        map((resData) => resData.places),
+        catchError((err) => {
+          console.log(err);
+          return throwError(() => new Error('Something went wrong fetching the available places. Please try again later.'));
+        })
       )
       .subscribe({
         next: (places) => {
           this.places.set(places);
         },
-        error: (err) => {
-          console.log(err);
-          this.error.set('Something went wrong fetching the available places. Please try again later.');
+        error: (err: Error) => {
+          this.error.set(err.message)
         },
         complete: () => {
           this.isFetching.set(false);
